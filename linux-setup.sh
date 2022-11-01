0e2a07231fb26668abe9fa2bb6401400d02f6106
Derek Briggs <contact@derekbriggs.xyz>
2022-10-31 22:38:33 -0400

    Remove references to Atom

--- linux-setup.sh
+++ linux-setup.sh
@@ -46,13 +46,6 @@ fi
 #these two scripts used to be one. But moving certain things that didn't require sudo rights, into their own script 
 #just makes things better.
 
-#This command installs the atom packages I use. It doesn't require sudo rights and is platform agnostic, so I just put it here.
-#to add/remove packages that get installed here, just edit the atom-package-list.txt file
-#you probably will want to make changes in that file, because some of those packages do have system requirements. For example language-elixir gave me errors until I actually installed elixir on my system.
-#obviously comment or delete this line if you didn't install atom.
-#running the command "apm list --installed --bare > atom-package-list.txt" will help you easily generate a file like this, on a system where you already have atom set up.
-apm install --packages-file atom-package-list.txt
-
 #make sure npm is at the latest possible version. Requires you to have installed nodejs.
 sudo npm install npm -g
 
